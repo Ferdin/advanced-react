f76f418dd4f7fd79e7306c63518d1578c39c3160
multimatic-ferdin <fnorbert@multimatic.com>
2024-11-14 08:26:17 -0500

    add react router

--- worldwise/src/pages/PageNotFound.jsx
+++ worldwise/src/pages/PageNotFound.jsx
@@ -1,9 +1,10 @@
-function PageNotFound() {
+import PageNav from "../components/PageNav";
+
+export default function PageNotFound() {
   return (
     <div>
-      <h1>Page Not Found</h1>
+      <PageNav />
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
